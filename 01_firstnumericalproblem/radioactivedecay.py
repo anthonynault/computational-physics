060ea4810e24c91aef944322f065a3581a2d91e8
anthonynault <anault@Anthonys-MacBook-Pro.local>
2020-11-15 12:13:17 -0800

    add pyproject

--- 01_firstnumericalproblem/radioactivedecay.py
+++ 01_firstnumericalproblem/radioactivedecay.py
@@ -1,18 +1,39 @@
-from dataclasses import dataclass
-
-
-nuclei: list[float] = [100]
-time: list[float] = [0]
-data: dict[float, float] = {}
+ 
+ # time step used in caluating differential decay
 dt: float = 0.05
 
-def decay(nuclei: float, time: float) -> dict[float, float]:
-    if (time >= 5): return data
-    else:
-        data[time] = nuclei
-        remaining_nuclei = nuclei - (nuclei * dt)
-        return decay(remaining_nuclei, time + dt)
-    
-results = decay(nuclei=100, time=0.0)
+# the time at which to end the decay simulation
+end_time: float = 5.0
 
-for time, nuclei in results.items(): print(time, nuclei)
+# store results (time -> remaning nuclei)
+data: dict[float, float] = {}
+
+total_steps = end_time / dt
+
+def radioactive_decay(nuclei: float, time: float = 0.0) -> dict[float, float]:
+    """Radioactive decay calculation.
+
+    Computes change in quantiy of nuclei over time as a result of radioactive decay.
+    The current implementation uses the euler method and tail recursion.
+
+    Args:
+        nuclei: Initual quantity of nuclei. As fractions of nucli may not make sense
+            this may be interpreted as a continuous mass distribution.
+        time: the current time from which to compute the change in remaining nuclei.
+
+    Returns:
+        A map of time to remaning nucei.
+
+    Example:
+        >>> results = decay(nuceli=100.0)
+        >>> results[1]
+        >>> ???
+    """
+    data[time] = nuclei
+    
+    if (time >= end_time): return data
+    else:
+        remaining_nuclei = nuclei * (1 - dt)
+        return radioactive_decay(remaining_nuclei, time + dt)
+    
+results = radioactive_decay(nuclei=100.0, time=0.0)
